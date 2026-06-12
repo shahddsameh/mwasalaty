eccfacd7a3f8b78bedb55c1f7e2246f799ffc033
sarahendwy <111707392+sarahendwy@users.noreply.github.com>
2026-06-13 00:59:09 +0300

    commit

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-0791036e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.1qct07i7les"
+    "revision": "0.3i5c167e46s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {

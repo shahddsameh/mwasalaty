cbe22c0578c90f8c2f4fb8fc8a978c727b084dd5
Renad Ibrahem <ibrahemrenad00@gmail.com>
2026-06-17 05:09:22 +0300

    add improvments on User Interface

--- frontend/src/components/route/RouteCard.vue
+++ frontend/src/components/route/RouteCard.vue
@@ -14,7 +14,7 @@
       >
         <Zap class="w-3 h-3 md:w-4 md:h-4" />
         <span class="hidden sm:inline">{{ t("routeResults.fastest") }}</span>
-        <span class="sm:hidden">Fastest</span>
+        <span class="sm:hidden">{{ t("routeResults.fastest") }}</span>
       </span>
 
       <span
@@ -23,17 +23,15 @@
       >
         <Wallet class="w-3 h-3 md:w-4 md:h-4" />
         <span class="hidden sm:inline">{{ t("routeResults.cheapest") }}</span>
-        <span class="sm:hidden">Cheap</span>
+        <span class="sm:hidden">{{ t("routeResults.cheapest") }}</span>
       </span>
       <span
         v-if="isComfortable"
         class="flex items-center gap-1.5 px-3 py-2 md:px-5 md:py-3 rounded-full bg-blue-200 text-blue-900 text-xs font-semibold"
       >
         <Sofa class="w-3 h-3 md:w-4 md:h-4" />
-        <span class="hidden sm:inline">{{
-          t("routeResults.mostComfortable")
-        }}</span>
-        <span class="sm:hidden">Comfy</span>
+        <span class="hidden sm:inline">{{ t("routeResults.mostComfortable") }}</span>
+        <span class="sm:hidden">{{ t("routeResults.mostComfortable") }}</span>
       </span>
     </div>
     <!-- Transport Steps -->
